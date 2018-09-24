684c1307d50c70742604e973d2f8ba0ba73a3199
bola <bola.ringley.co.uk>
2018-09-24 18:10:58 +0100

    building logic

---
+++ src/components/LeftSelectBox.js
@@ -0,0 +1,55 @@
+import React , { Component } from 'react';
+import PropTypes from 'prop-types';
+
+
+class LeftSelectBox extends Component {
+
+  state ={
+    rate_from: 0,
+    amountToConvert : 0,
+  };
+
+  componentDidMount =() => {
+    this.setState({
+      rate_from: this.selectValue.name,
+    })
+  };
+
+  onSelectChangeValue = (e) => {
+    this.setState({
+      rate_from: e.target.options[e.target.selectedIndex].text
+    })
+  };
+
+
+	render(){
+    return(
+      <div className="card-container"> 
+       <p className="per-rate">{`1 ${this.state.rate_from} = ${this.props.rateTo}` }</p>
+       <div  className="card">
+          <select ref={(select) => this.selectValue = select }  onChange={this.onSelectChangeValue} name='USD'>
+            {
+              this.props.ratesInEuro.map((rates , index) => {
+                return <option
+                          value={rates.rate} 
+                          key={index}
+                          name={rates.currency}
+                          > 
+                          {rates.currency}
+                        </option>
+              })
+            }
+          </select>
+          <input type="text" placeholder="type amount to convert here" ref={(input) => this.inputValue = input }/>
+       </div>
+      </div>
+   );
+  };
+}
+
+LeftSelectBox.propTypes={
+  ratesInEuro : PropTypes.array.isRequired,
+  rateTo : PropTypes.number.isRequired,
+};
+
+export default LeftSelectBox ;
---
+++ src/components/RightSelectBox.js
@@ -0,0 +1,46 @@
+import React , { Component } from 'react';
+import PropTypes from 'prop-types';
+
+
+class RightSelectBox extends Component {
+
+  state ={
+    rate_from: '',
+    rate_to : 0,
+    amountToConvert : 0,
+  }
+
+  componentDidMount = () => {
+    this.selectChange();
+  }
+
+  selectChange = () => {
+    this.props.onSelectChange(this.selectValue.value);
+  }
+
+
+	render(){
+    return(
+      <div className="card-container"> 
+       <p className="per-rate"></p>
+       <div  className="card">
+          <select onChange={this.selectChange} ref={(select) => this.selectValue = select } name='USD'>
+            {
+              this.props.ratesInEuro.map((rates , index) => {
+                return <option value={rates.rate} key={index}> {rates.currency} </option>
+              })
+            }
+          </select>
+          <input type="text" placeholder="type amount to convert here" ref={(input) => this.inputValue = input }/>
+       </div>
+      </div>
+   );
+  }
+}
+
+RightSelectBox.propTypes={
+  ratesInEuro : PropTypes.array.isRequired,
+  onSelectChange: PropTypes.func.isRequired
+};
+
+export default RightSelectBox ;
